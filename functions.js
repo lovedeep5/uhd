594d5c5b584d778ace10f97482c08a587b608879
lovedeep5 <lovedeep5.abh@gmail.com>
2021-04-18 18:53:53 +0530

    FORM VALIDATION ADDED

--- functions.js
+++ functions.js
@@ -32,7 +32,39 @@ function scrollFunction() {
 // FORM VALIDATION
 $(".user-request-form").submit(function(e){
   e.preventDefault();
-alert(e.target.firstName.value);
+var first_name = e.target.firstName.value;
+var last_name = e.target.lastName.value;
+var user_email = e.target.email.value;
+var user_phone = e.target.phone.value;
+
+var program_of_interest = e.target.programOfInterest.value;
+var program_delivery_type = e.target.programDeliveryType.value;
+var preferred_communication_type = e.target.preferredCommunicationType.value;
+
+if(first_name.length <= 3) {
+  alert("JS VALIDATION ALERT: FIRST NAME SHOULD BE MORE THAN 3");
+  return false;
+} else if (last_name <= 3 ) {
+  alert("JS VALIDATION ALERT: LAST NAME SHOULD BE MORE THAN 3");
+  return false;
+}  else if (user_phone <= 9 ) {
+  alert("JS VALIDATION ALERT: PHONE SHOULD BE MORE THAN OR EQUAL 10");
+  return false;
+} else if (program_of_interest == false ) {
+  alert("JS VALIDATION ALERT: Please Select Program of interest");
+  return false;
+}else if (program_delivery_type == false ) {
+  alert("JS VALIDATION ALERT: Please Select Program Delivery Type");
+  return false;
+}else if (preferred_communication_type == false ) {
+  alert("JS VALIDATION ALERT: Please Select Preferred Communication Type");
+  return false;
+} else {
+  alert("Thank you for submitting the details, you will be redirected to netsolutions.com");
+  window.location.href = "https://www.netsolutions.com/";
+}
+
+
  
 })
 // FORM VALIDATION
